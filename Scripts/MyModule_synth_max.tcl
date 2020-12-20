9bb361c2f757d68810d9c3e2adb9214be07af76f
Ivanov9090 <73597484+Ivanov9090@users.noreply.github.com>
2020-12-20 20:19:53 +0300

    Synthesis
    Файл для логического синтеза для fast corner

--- Scripts/MyModule_synth_max.tcl
+++ Scripts/MyModule_synth_max.tcl
@@ -26,4 +26,3 @@ write_hdl -mapped > ../Outputs/Synthesis/CIC_synth_max.v
 ## Write out the SDC file we will take into the place n route tool
 write_sdc > ../Outputs/Synthesis/CIC_out_max.sdc
 gui_show
-
